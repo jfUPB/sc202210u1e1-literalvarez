196db4782f9ab5626aab691e3667fc81ded915e6
Tomas Alvarez <tomas1alvares@gmail.com>
2022-02-12 21:51:46 -0500

    F Loop Error
    Pinta de negro pero el loop no cierra bien cuando termina de pintar de negro

--- problem/program.asm
+++ problem/program.asm
@@ -1,6 +1,37 @@
-// COLOCO AQUÍ TU NOMBRE COMPLETO
-// COLOCA AQUÍ TU ID
-// COLOCA AQUÍ TU CORREO ELECTRÓNICO
+// Tomas lvarez rtinez
+// 000396822
+// tomas1alvares@gmail.com
+
 (START)
     @START
-    0;JMP
+    D=M
+    @1
+    M=-1
+(LOOP READ)
+    @70
+    M=A
+    D=M
+    @24576
+    D=D-M
+    @4
+    D;JNE
+(LOOP F)
+    @16384
+    D=A
+    M=-1
+    @3
+    M=D
+    @3
+    D=M
+    @1
+    D=D-M
+    A=D
+    M=-1
+    @3
+    M=D 
+    @24575
+    D=M+1
+    @16
+    0;JEQ
+    
+
